81c776f63c5b4d6892be41adf94ab52d67a46554
BarakovaES <barakova.2000@mail.ru>
2021-10-11 08:41:31 +0400

    init puzzle helper

---
+++ OpenCvPuzzleHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace OpenCvPuzzleHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "C:\\Users\\Елена\\Desktop\\fqefqf.png";
+            Image<Bgr, byte> image = new Image<Bgr, byte>(fileName);
+            CvInvoke.Imshow("image", image);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
