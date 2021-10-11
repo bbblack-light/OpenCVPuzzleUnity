482919c47cff6f89aa85f75a3e17437aba59b998
BarakovaES <barakova.2000@mail.ru>
2021-10-11 12:12:16 +0400

    init unity project

---
+++ SimplePuzzle/Assets/Scripts/SelectFileButton.cs
@@ -0,0 +1,20 @@
+﻿using SFB;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectFileButton : MonoBehaviour
+{
+    private Button _button;
+
+    void Start()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(Call);
+    }
+
+    private void Call()
+    {
+        var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false);
+        Debug.Log(paths[0]);
+    }
+}
