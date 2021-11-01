e14b4cd2d1d61a6d5a98606847d13b2205b70294
BarakovaES <barakova.2000@mail.ru>
2021-11-01 07:51:52 +0400

    corrected work with files & instructions added

--- OpenCvPuzzleHelper/MainListener.cs
+++ OpenCvPuzzleHelper/MainListener.cs
@@ -4,10 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using DefaultNamespace;
-using Emgu.CV;
-using Emgu.CV.Structure;
 using Newtonsoft.Json;
 
 namespace OpenCvPuzzleHelper
@@ -16,22 +13,12 @@ namespace OpenCvPuzzleHelper
     {
         private static FullParts fullParts;
 
-        //for receive
         private static TcpListener Server;
         private static NetworkStream stream;
         private static List<string> filenames = new List<string>();
         static void Main(string[] args)
         {
-            ImageParameters parameters = new ImageParameters()
-            {
-                path = "C:\\Users\\Елена\\Desktop\\therg wolf deathloop.jpg",
-                rows = 2,
-                cols = 1
-            };
-
-            // ImageProcessing.CutImage(parameters);
-
-            //TODO 11 October 2021 г.: return for listen server
+            
             StartListen();
         }
 
@@ -39,7 +26,6 @@ namespace OpenCvPuzzleHelper
         {
             FileStream file;
             IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-            int port = 8888;
             Server = new TcpListener(localAddr, 11000);
             Server.Start();
             Console.WriteLine("Server started.\nWaiting for images...");
@@ -52,7 +38,6 @@ namespace OpenCvPuzzleHelper
                     
                     var receiveData = new byte[1024];
                     stream.Read(receiveData, 0, receiveData.Length);
-                    // if (client.Available == 0) continue;
                     var message = Encoding.ASCII.GetString(receiveData, 0, receiveData.Length);
                     Console.WriteLine("пришли параметры: " + message);
                     if (message.Contains("path"))
@@ -60,16 +45,14 @@ namespace OpenCvPuzzleHelper
                         ImageParameters imageParameters = JsonConvert.DeserializeObject<ImageParameters>(message);
                         fullParts = ImageProcessing.CutImage(imageParameters);
                         var resString = JsonConvert.SerializeObject(fullParts);
-                        if (File.Exists(fullParts.filename + ".txt"))
+                        if (File.Exists("my.txt"))
                         {
-                            File.Delete(fullParts.filename + ".txt");
+                            File.Delete("my.txt");
                         }
                         
-                        file = File.Create(fullParts.filename + ".txt");
+                        file = File.Create("my.txt");
                         file.Write(Encoding.ASCII.GetBytes(resString));
                         Console.WriteLine(file.Name);
-                        // filename = AppDomain.CurrentDomain.BaseDirectory + file.Name;
-                        filenames.Add(file.Name);
                         var filenameBytes = Encoding.ASCII.GetBytes(file.Name);
                         stream.Write(filenameBytes, 0, filenameBytes.Length);
                         file.Close();
@@ -83,13 +66,6 @@ namespace OpenCvPuzzleHelper
             }
             finally
             {
-                foreach (var filename in filenames)
-                {
-                    if (filename != String.Empty && File.Exists(filename))
-                    {
-                        File.Delete(filename);
-                    }
-                }
                 Console.WriteLine("Server closed");
             }
         }
